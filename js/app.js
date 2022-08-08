58060a0e5d6860d70b52286cb2a208a35aac5a7f
Luis Lanca <luis_lanca@outlook.com>
2022-08-08 00:03:17 -0300

    Creating API service

--- js/app.js
+++ js/app.js
@@ -12,12 +12,33 @@ async function fetchScructure(url) {
 }
 
 function createTemplate(template) {
+  erasePage();
   var temp = document.querySelector(template);
   var clon = temp.content.cloneNode(true);
   document.body.querySelector("main").appendChild(clon);
 }
 
 async function getUsers() {
+  var main = document.querySelector("main");
   let users = await fetchScructure("/users");
-  return console.log(users)
+  users.forEach((user) => {
+    let li = document.createElement("li");
+    li.innerText = `${user.name.firstname} ${user.name.lastname}`;
+    main.appendChild(li);
+  });
+}
+
+async function getWinners() {
+  var main = document.querySelector("main");
+  let users = await fetchScructure("/users?limit=3");
+  users.forEach((user, i) => {
+    let li = document.createElement("li");
+    li.innerText = `${(i += 1)}- ${user.name.firstname} ${user.name.lastname}`;
+    main.appendChild(li);
+  });
+}
+
+function erasePage() {
+  var main = document.querySelector("main");
+  main.innerHTML = "";
 }
